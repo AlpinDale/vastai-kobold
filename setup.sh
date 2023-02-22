5fe0a9a228448de83b75b541d33b6e3b3ea243ab
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2023-02-22 23:11:07 +0430

    Minor fixes and experiments

--- setup.sh
+++ setup.sh
@@ -20,11 +20,11 @@ fi
 mkdir AI && cd AI
 git clone https://github.com/henk717/KoboldAI
 cd KoboldAI
-tmux
-tmux split-window -v
-sudo apt-get install -y rustc cargo
-cargo install bore-cli
-bore local 5000 --to bore.pub
-tmux select-pane -t 0
-./play.sh
+tmux new-session -d -s asession
+tmux send-keys 'tmux split-window -v' C-m
+tmux send-keys 'apt-get install -y rustc cargo' C-m
+tmux send-keys 'cargo install bore-cli' C-m
+tmux send-keys 'bore local 5000 --to bore.pub' C-m
+tmux send-keys 'tmux select-pane -t 0' C-m
+tmux send-keys './play.sh' C-m
 ```
