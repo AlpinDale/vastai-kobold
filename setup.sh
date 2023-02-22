5f21b5a9b4207d464eccfb387593fc47fc6b51ca
AlpinDale <52078762+AlpinDale@users.noreply.github.com>
2023-02-23 04:05:01 +0430

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-if ! dpkg -s nvidia-driver >/dev/null 2>&1; then
-  echo "Installing Nvidia drivers..."
-  apt-get update
-  apt-get install -y nvidia-driver
-fi
-if ! dpkg -s cuda >/dev/null 2>&1; then
-  echo "Installing CUDA..."
-  apt-get install -y nvidia-cuda-dev nvidia-cuda-toolkit
-fi
-if ! dpkg -s git >/dev/null 2>&1; then
-  echo "Installing Git..."
-  apt-get install -y git
-fi
-if ! dpkg -s tmux >/dev/null 2>&1; then
-  echo "Installing Tmux..."
-  apt-get install -y tmux
-fi
+
+# Create directory AI and navigate into it
 mkdir AI && cd AI
+
+# Clone the KoboldAI repository
 git clone https://github.com/henk717/KoboldAI
+
+# Navigate into the KoboldAI directory
 cd KoboldAI
-tmux new-session -d -s asession
-tmux send-keys 'tmux split-window -v' C-m
-tmux send-keys -t 1 'apt-get install -y rustc cargo' C-m
-tmux send-keys -t 1 'cargo install bore-cli' C-m
-tmux send-keys -t 1 'bore local 5000 --to bore.pub' C-m
-tmux send-keys -t 0 './play.sh' C-m
+
+# Install Rust and Cargo
+apt-get install -y rustc cargo
+
+# Install bore-cli using Cargo
+cargo install bore-cli
+
+# Create a public tunnel for port 5000 using bore.pub
+bore local 5000 --to bore.pub | tee bore_output.txt
+
+# Extract the public URL from the output
+url=$(grep -o "https://.*" bore_output.txt)
+
+# Print the public URL
+echo "Public URL: $url"
+
+# Launch the play.sh script in the background
+nohup ./play.sh &
+
